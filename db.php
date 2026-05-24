7b1dadbe0102a566e7eac0a3c74ac3019b0d286e
sheena <anorasheenamae19@gmail.com>
2026-05-24 17:21:37 +0800

    Created API database connection

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "coffee_shop_db";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+echo "API database connected successfully.";
+
+?>
